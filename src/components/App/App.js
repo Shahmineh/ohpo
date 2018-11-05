02d0517cbd916e952ed68c513c3fa1aa50ea2bc1
PaulinaHT <paulina.hertor@gmail.com>
2018-11-05 14:30:27 +0100

    Moved footer from Component App to Component Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,4 @@
 import './App.scss';
-import imgLinkedIn from './images/linkedin-sign.png';
-import imgMail from './images/mail-black-envelope-symbol.png';
 
 @observer export default class App extends Component {
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+<Fragment>
+  <footer className = "footer bg-light mt-3">
+    <div>
+      <Link to="/" className = "footer__links" style = {{textDecoration:'none'}}><img alt="imgMail" className="footer__linkImage" src={imgMail} />
+      <small className = "footer__link">Info@ohpokonsult.se</small>
+      </Link>
+    </div>
+    <div>
+      <Link to="/clock" className = "footer__links"style = {{textDecoration:'none'}}><img alt="imageLinkedIn" className="footer__linkImage" src={imgLinkedIn} /><small className="footer__link">OHPO Konsult</small>
+      </Link>
+    </div>
+  </footer>
+</Fragment>
