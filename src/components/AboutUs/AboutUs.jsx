e9e25fa797cbb6cf5a2ea34a61179e28744b0ddd
PaulinaHT <paulina.hertor@gmail.com>
2018-11-12 01:57:08 -1100

    AboutUs section styling and responsivness

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,18 +1,18 @@
 <Fragment>
   <h2 className = "aboutUs__headline">Om oss</h2>
     <Row> 
-      <Col sm="4">
-        <img alt="imgExcel" className="aboutUs__imgExcel" src={imgExcel} />
+      <Col sm="4" className="aboutUs__img">
+        <img alt="imgExcel" className="aboutUs__imgs" src={imgExcel} />
       </Col>
-      <Col sm="8">
+      <Col sm="8" className="aboutUs__text">
         <p>
         Lorem ipsum dolor sit amet consectetuer adipiscing elit. Non integer fames nascetur convallis morbi posuere id morbi metus. Ad magna accumsan nam. Condimentum etiam ridiculus sed purus. Suspendisse nec cras eni eros proin curabitur commodo ac eros ut a duis at. Diam dictum parturient vitae torquent dui eu consectetuer posuere netus pede habitant proin. Sit egestas neque augue sapien semper ullamcorper ut euismod massa dignissim vitae. Mi montes placerat adipiscing fames nulla sodales pharetra. Arcu duis phasellus tellus a potenti. Id est mi rutrum tincidunt placerat lobortis potenti est habitasse nulla augue.
         Penatibus phasellus lectus diam. Velit porta. Hac nunc elit eros urna suscipit adipiscing. Arcu urna. Cum consequat taciti vel odio porta porta aliquam vehicula libero. Nam adipiscing nam. Odio interdum molestie justo vitae cursus fusce urna integer etiam in a aliquet. Dolor penatibus dictumst blandit parturient ipsum magna ullamcorper mus. Montes cras. Posuere non sapien varius eleifend massa et amet. Cras ultricies ad mi nostra fringilla parturient vestibulum nibh class mauris at placerat nunc sociis. Et torquent iaculis. Laoreet tempor ac in non odio eu pellentesque interdum.</p>
       </Col>
     </Row>  
 
-    <Row>
-      <Col>
+    <Row className="aboutUs__row">
+      <Col className="aboutUs__text">
         <p>
         Mi nisi habitasse adipiscing lorem litora scelerisque lectus malesuada. Blandit ligula risus dictum est. Diam consectetuer montes quisque porttitor nam fringilla purus aptent quis. Augue hac adipiscing dolor ve cursus duis praesent. Curae at pellentesque dictumst porta duis. Molestie nibh mi vulputate lacus nam nisl metus. Porta elementum nulla eu phasellus vehicula massa amet faucibus gravida elit rhoncus taciti aenean non. Hac pede primis nulla fames lacus potenti leo tortor nam. Egestas non sodales nulla vulputate convallis mollis.
         Ridiculus montes euismod hymenaeos ante. Cubilia varius. Aliquam parturient pede habitant vestibulum. Faucibus tortor odio ac mattis per tellus tempus magna. Integer risus. Dapibus tristique augue vulputate sagittis platea litora parturient nunc fusce platea ut faucibus ante mattis. Ut eleifend iaculis aliquam platea eni. Molestie erat urna penatibus iaculis urna urna quisque velit id lobortis morbi turpis cras. Risus elementum pellentesque vestibulum felis primis dictum mauris torquent odio posuere hac semper. Nunc proin parturient ridiculus ante cum? Nisl. Litora et integer sollicitudin vehicula sagittis iaculis ornare maecenas duis rhoncus. Mus cursus nibh etiam adipiscing ultricies. Duis vel arcu congue. Mi dapibus sollicitudin sociosqu sociosqu rhoncus.
@@ -20,11 +20,11 @@
       </Col>
     </Row>
 
-    <Row> 
-      <Col sm="4">
-        <img alt="imgTeam" className="aboutUs__imgTeam" src={imgTeam} />
+    <Row className="aboutUs__row"> 
+      <Col sm="4" className="aboutUs__img">
+        <img alt="imgTeam" className="aboutUs__imgs" src={imgTeam} />
       </Col>
-      <Col sm="8">
+      <Col sm="8" className="aboutUs__text">
         <p>
         Lorem ipsum dolor sit amet consectetuer adipiscing elit. Non integer fames nascetur convallis morbi posuere id morbi metus. Ad magna accumsan nam. Condimentum etiam ridiculus sed purus. Suspendisse nec cras eni eros proin curabitur commodo ac eros ut a duis at. Diam dictum parturient vitae torquent dui eu consectetuer posuere netus pede habitant proin. Sit egestas neque augue sapien semper ullamcorper ut euismod massa dignissim vitae. Mi montes placerat adipiscing fames nulla sodales pharetra. Arcu duis phasellus tellus a potenti. Id est mi rutrum tincidunt placerat lobortis potenti est habitasse nulla augue.
         Penatibus phasellus lectus diam. Velit porta.</p>
