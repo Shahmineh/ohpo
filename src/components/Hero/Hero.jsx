728eb68670f408979ec1ba831faf5e776f9efa4e
Shamin Krook <shahmineh.gh@hotmail.com>
2018-11-12 12:23:15 +0100

    Add hero text

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,3 +1,9 @@
 <Fragment>
-  <img alt="Computer with excel pic on" className="hero__image" src={hero}/>
+  <div className="hero">
+    <img alt="Computer with excel pic on" className="hero__image" src={hero}/>
+    <p className="hero__imageText">
+      HELPS COMPONIES WORK SMARTER <br /> WITH MICROSOFT EXCEL SPREADSHEETS
+    </p>
+    
+  </div>
 </Fragment>
