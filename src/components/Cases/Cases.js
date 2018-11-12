75eb9fba8ab206ad5850a49030183bb9d87d8140
PaulinaHT <paulina.hertor@gmail.com>
2018-11-12 02:53:05 -1100

    Import images to cases section

---
+++ src/components/Cases/Cases.js
@@ -0,0 +1,11 @@
+import './Cases.scss';
+import caseIkea from './img/ikea.jpg';
+import caseBank from './img/bank.jpg';
+import case1 from './img/case1.png';
+import case2 from './img/case2.png';
+import case3 from './img/case3.png';
+import case4 from './img/case4.png';
+import case5 from './img/case5.png';
+import case6 from './img/case6.png';
+
+export default class Cases extends Component {}
