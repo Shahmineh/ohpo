fda9eb50fce07918a71ed088058ec221287d0180
Shamin Krook <shahmineh.gh@hotmail.com>
2018-11-12 14:52:50 +0100

    Add text and some styling to contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,20 @@
 <Fragment>
-  <div>
-  
+  <h2 className="contact__headline text-center mt-5 mb-4">Let's get in touch</h2>
+  <p className="contact__text text-center">
+    Ready to start your next procect with us? That´s great!
+    Give us a call or send us an email and we will get back to 
+    you as soon as possible!
+  </p>
+  <div className="">
+    <span className="block">
+      <img src={linkedin} className="contact__linkedin" alt="" />
+      <p>info@ohpokonsult.se</p>
+    </span>
+    <span className="block">
+      <img src={mail} className="contact__mail" alt="" />
+      <p>OHPO konsult</p>
+    </span>
+    
   </div>
+  
 </Fragment>
