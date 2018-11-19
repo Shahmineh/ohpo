64ba0d13735345f05e78d0d9f9ce64b4efdbcb0c
Shamin Krook <shahmineh.gh@hotmail.com>
2018-11-19 14:17:54 +0100

    moved text to center with smaller col

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,7 @@
-<div className="contact">
+<div className="contact" style={{display:'inline-block'}}>
   <h2 className="contact__headline text-center mt-5 mb-4">Let's get in touch</h2>
   <p className="contact__text text-center pb-3">
-    Ready to start your next procect with us? That´s great!
+    Ready to start your next project with us? That´s great!
     Give us a call or send us an email and we will get back to 
     you as soon as possible!
   </p>
