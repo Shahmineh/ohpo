fc95ad23692adfa22574febd745f368072cd791f
Shamin Krook <shahmineh.gh@hotmail.com>
2018-11-26 10:40:09 +0100

    Fixed some errors with scrollchor

--- src/components/MainNav/MainNav.jsx
+++ src/components/MainNav/MainNav.jsx
@@ -2,27 +2,24 @@
   <Navbar light expand="md"
   style={{padding: '20px', backgroundColor: '#fff'}} className="fixed-top">
     <NavbarBrand  to="/">
-      {/* <img alt="React" className="logo" src={logo} /> */}
       <h2 className="navigation__logo">OHPO Konsult</h2>
     </NavbarBrand>
     <NavbarToggler onClick={e => this.toggle()} />
     <Collapse isOpen={this.isOpen} navbar>
       <Nav className="ml-auto" navbar>
         <NavItem>
-          <NavLink><Scrollchor exact to="#about-us" activeClassName = "active">Om oss</Scrollchor></NavLink>
+          {/* Scrollchor: A React component for scroll to #hash links with smooth animations */}
+          <Scrollchor exact="true" to="#about-us" className="nav-link">Om oss</Scrollchor>
         </NavItem>
         <NavItem>
-          <NavLink><Scrollchor to="#contact" activeClassName = "active">Kontakta oss</Scrollchor></NavLink>
+          <Scrollchor to="#contact" className="nav-link">Kontakta oss</Scrollchor>
         </NavItem>
         <NavItem>
-          <NavLink><Scrollchor to="#cases" activeClassName = "active">Cases</Scrollchor></NavLink>
+          <Scrollchor to="#cases" className="nav-link">Cases</Scrollchor>
         </NavItem>
         <NavItem>
-          <NavLink to="/conditionals" activeClassName = "active">In English</NavLink>
+          <NavLink to="/conditionals">In English</NavLink>
         </NavItem>
-        {/* <NavItem>
-          <NavLink to="/loops" activeClassName = "active">Loops</NavLink>
-        </NavItem> */}
       </Nav>
     </Collapse>
   </Navbar>
