eb3e358494db1d57b875b2f82f7bc92606d21857
Shamin Krook <shahmineh.gh@hotmail.com>
2019-01-08 10:14:58 +0100

    Fix some text

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 <div className="contact" id="contact" style={{display:'inline-block'}}>
   <h2 className="contact__headline text-center mt-5 mb-4">Let's get in touch</h2>
   <p className="contact__text text-center pb-3">
-    Ready to start your next project with us? That´s great!
+    Ready to start your next project with us? That's great!
     Give us a call or send us an email and we will get back to 
     you as soon as possible!
   </p>
@@ -9,14 +9,16 @@
     <span className="d-inline-block mr-5">
       <a href="https://www.linkedin.com/company/ohpo-konsult-ab/?trk=cws-cpw-logo-0-0" style={{color: '#000'}}>
         <img src={linkedin} className="contact__linkedin" alt="" />
-         <p>info@ohpokonsult.se</p>
+         <p>OHPO konsult</p>
       </a>
       
     </span>
     <span className="d-inline-block ml-1">
       <a href="mailto:info@ohpokonsult.se" style={{color: '#000'}}>
         <img src={mail} className="contact__mail" alt="" />
-        <p>OHPO konsult</p>
+        <p>
+          info@ohpokonsult.se
+        </p>
       </a>
       
     </span>
