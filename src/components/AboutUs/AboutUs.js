7df67cca5192d500a58e14c564b0a84ab0e064e2
Shamin Krook <shahmineh.gh@hotmail.com>
2019-01-21 11:46:08 +0100

    Add new image

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,5 +1,6 @@
 import './AboutUs.scss';
 import imgExcel from './image/excel.jpg';
 import imgTeam from './image/team.jpg';
+import teamwork5 from './image/teamwork5.jpg';
 
 export default class AboutUs extends Component {}
